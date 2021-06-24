9855916120d826f39c9202ac907b61004ce559ef
mohammad ibraighith <mohammad.ibreighith@hotmail.com>
2021-06-24 12:22:05 +0300

    first commit

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    'mongodb+srv://user:user123@cluster0.gztky.mongodb.net/test?retryWrites=true&w=majority',
-};
--- server.js
+++ server.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const fs = require('fs');
-// const Pool = require('pg').Pool;
-
-// const pool = new Pool();
-// var sql = fs.readFileSync('./DB/database.sql').toString();
-
-app.set('trust proxy', true);
+const PORT = process.env.PORT || 3100;
+const path = require('path');
+const publicPath = path.join(__dirname, 'client/public');
 
 app.use(cors());
-
-const todoRoute = require('./routes/todolist');
-
 app.use(express.json());
+app.use(express.static(publicPath));
+const todoRoute = require('./routes/todolist');
 
 app.use('/todolist', todoRoute);
 
-// console.log(sql);
-// pool.connect((err, client, release) => {
-//   if (err) {
-//     return console.error('Error acquiring client', err.stack);
-//   }
-//   client.query('SELECT FROM pg_database WHERE datname = tododb', (err, result) => {
-//     release();
-//     if (err) {
-//       return console.error('Error executing query', err.stack);
-//     }
-//     console.log(result.rows);
-//   });
-// });
+app.get('*', (req, res) => {
+  console.log(req);
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-app.listen(3100);
+app.listen(PORT);
