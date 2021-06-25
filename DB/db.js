094192a6060b3f100e384fb7c3afd962880c9cb8
mohammad bassam ibreighith <40917993+mohammadibreighith@users.noreply.github.com>
2021-06-25 18:46:46 +0300

    procfile

--- DB/db.js
+++ DB/db.js
@@ -1,13 +1,14 @@
 const pg = require('pg');
 
 require('dotenv').config();
+
 const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
-  ssl: false,
+  ssl: { rejectUnauthorized: false },
 };
 
 const client = new pg.Client(devConfig);
