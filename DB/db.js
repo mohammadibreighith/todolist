8974e5e638c917a7cef4c8387c554396cbd38dc9
mohammad bassam ibreighith <40917993+mohammadibreighith@users.noreply.github.com>
2021-06-25 14:50:44 +0300

    procfile

--- DB/db.js
+++ DB/db.js
@@ -1,11 +1,32 @@
-const Pool = require('pg').Pool;
+// const Pool = require('pg').Pool;
+var pg = require('pg');
+
 require('dotenv').config();
 
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// };
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const proConfig = process.env.DATABASE_URL;
 
-const pool = new Pool({
-  connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
-});
+// const pool = new Pool({
+//   connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+// });
+const client = new pg.Client(devConfig);
+client.connect();
 
-module.exports = pool;
+// console.log(pool);
+console.log(devConfig);
+console.log(proConfig);
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// });
+module.exports = client;
